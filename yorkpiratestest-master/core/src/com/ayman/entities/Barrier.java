246dbc0bf49aa83618feb02a2c6c8fbd14bb35d6
aymanish <80852613+aymanish@users.noreply.github.com>
2022-02-01 06:05:00 +0000

    Base Game with Barrier fully implemented (not refactored)
    Still need to refactor and add extra stuff: Health Bars, etc

---
+++ yorkpiratestest-master/core/src/com/ayman/entities/Barrier.java
@@ -0,0 +1,11 @@
+package com.ayman.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Barrier extends GameObject {
+    public Sprite barrier;
+
+    public Barrier() {
+        barrier = textureAtlas.createSprite("barrier");
+    }
+}
---
+++ yorkpiratestest-master/core/src/com/ayman/entities/Bullet.java
@@ -0,0 +1,59 @@
+package com.ayman.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Bullet extends GameObject {
+
+    private float timeLimit;
+    private float timer;
+
+    public Sprite bulletSprite;
+    public Rectangle rectBullet;
+
+    //aymans code:
+    private boolean remove;
+
+    public Bullet(float x, float y, float radians) {
+
+        bulletSprite = textureAtlas.createSprite("bullet");
+
+        rectBullet = bulletSprite.getBoundingRectangle();
+
+        width = height = 18;
+        this.x = x;
+        this.y = y;
+        this.radians = radians;
+
+        //set bullet speed
+        float speed = 350;
+        dx = MathUtils.cos(radians)*speed;
+        dy = MathUtils.sin(radians)*speed;
+
+        timer = 0;
+        timeLimit = 2;
+    }
+
+    public boolean shouldRemove() {return (remove);}
+
+
+    public void update(float dt){
+        x += dx * dt;
+        y += dy * dt;
+
+        bulletSprite.setX(x);
+        bulletSprite.setY(y);
+
+        rectBullet.x = x;
+        rectBullet.y = y;
+
+        timer += dt;
+        if (timer > timeLimit) {
+            remove = true;
+        }
+    }
+
+    //piblic void draw(ShapeRendered sr)
+}
