771651f9a970137be7a74c693315b9d5ad4b09a2
aymanish <80852613+aymanish@users.noreply.github.com>
2022-02-01 04:07:01 +0000

    Implemented barrier + objectives
    Need to create class for barrier which just holds the sprite but functionality of barrier is done. Colleges need to be places according to barrier.

---
+++ yorkpiratestest-master/core/src/com/ayman/entities/Constantine.java
@@ -0,0 +1,21 @@
+package com.ayman.entities;
+
+public class Constantine extends College {
+    public Constantine() {
+        x = 2000;
+        y = 900;
+        HP = 5;
+        POINTS = 500;
+        //set college sprite
+        png_npc = "ship_CT";
+        collegeSprite = textureAtlas.createSprite("constantine_island");
+        collegeSprite.setPosition(x, y);
+        //set bounding rectangle based on college sprite
+        boundRect = collegeSprite.getBoundingRectangle();
+        boundRect.x = x;
+        boundRect.y = y;
+        //set AOE
+        AOE.x = x+(width/2);
+        AOE.y = y+(height/2);
+    }
+}
