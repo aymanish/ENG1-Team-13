febfb86096acea0d531830aec8d394107d4cd045
aymanish <80852613+aymanish@users.noreply.github.com>
2022-02-01 03:11:19 +0000

    Added Objective Bar on top left
    new function drawObjectives() under MyGame

---
+++ yorkpiratestest-master/core/src/com/ayman/game/MyGame.java
@@ -0,0 +1,304 @@
+package com.ayman.game;
+import com.ayman.entities.*;
+import com.ayman.screen.TitleScreen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyGame extends Game {
+	//initialize shared resources: camera, font, spritebatch and game objects
+	public OrthographicCamera camera;
+	public BitmapFont font;
+	public SpriteBatch batch;
+	public Map map;
+	public PlayerShip player;
+	public College AnneLister, Constantine, Goodricke;
+	private ArrayList<College> collegeList = new ArrayList<>();
+	public ArrayList<Bullet> bullets;
+	public npcShip shipAL, shipGR, shipCT;
+
+	//COLLEGE AIM:
+	public Vector2 bulletDirection;
+	public float bulletRadians;
+
+	//start/end screen textures:
+	//public Texture titleScreen, endScreen;
+
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+
+		//camera setup: viewport 0.5 x smaller that game screen:
+		//viewport ratio should be same as game screen ratio to maintain aspect ratio/avoid distortion:
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		//initialize map
+		map = new Map();
+
+		//initialize players
+		bullets = new ArrayList<Bullet>();
+		player = new PlayerShip(bullets);
+
+		//initialize npc ships
+
+		//initialize colleges;
+		AnneLister = new AnneLister();
+		Constantine = new Constantine();
+		Goodricke = new Goodricke();
+
+		//AnneLister.spreadNPC();
+		//Constantine.spreadNPC();
+		//Goodricke.spreadNPC();
+
+		//initialize list of colleges
+		collegeList = new ArrayList<>();
+		collegeList.add(AnneLister);
+		collegeList.add(Goodricke);
+		collegeList.add(Constantine);
+
+		//initialize NPC:
+		for (College college : collegeList) {
+			for (int i = 0; i < college.npcCount; i++) {
+				college.NPCs.add(new npcShip(college.png_npc));
+				college.NPCs.get(i).x = college.x + (i*50) + 50;
+				college.NPCs.get(i).y = college.y - 100;
+			}
+		}
+
+
+		//set screen to title screen always upon startup:
+		setScreen(new TitleScreen(this));
+	}
+
+
+	public void updatePlayerBullets(float delta) {
+		//UPDATE PLAYER BULLETS:
+		for (College college: collegeList) {
+			for (int i = 0; i < player.bullets.size(); i++) {
+				player.bullets.get(i).update(delta);
+				//System.out.println("BULLET SHOT");
+				//COLLEGE FOR LOOP FUNCTION:
+				//for (College college: collegeList) {
+				if (player.bullets.get(i).rectBullet.overlaps(college.boundRect)) {
+					//game.player.bullets.get(i).BulletCollide = true;
+					college.isAttacked = true;
+					System.out.println("BULLET COLLIDE");
+				}
+				if ((player.bullets.get(i).shouldRemove())||(college.isAttacked)) {
+					player.bullets.remove(i);
+					i--;
+					System.out.println("BULLET REMOVED");
+				}
+			}
+		}
+	}
+
+	public void updateCollegeBullets(float delta) {
+		for (College college: collegeList) {
+			//UPDATE COLLEGE BULLETS:
+			for (int i = 0; i < college.bullets.size(); i++) {
+				college.bullets.get(i).update(delta);
+				if (college.bullets.get(i).rectBullet.overlaps(player.rectPlayer)) {
+					//game.player.bullets.get(i).BulletCollide = true;
+					player.isAttacked = true;
+					System.out.println("PLAYER BULLET COLLIDE");
+				}
+				if ((college.bullets.get(i).shouldRemove())||(player.isAttacked)) {
+					college.bullets.remove(i);
+					i--;
+					System.out.println("PLAYER BULLET REMOVED");
+				}
+			}
+		}
+	}
+
+	public void drawMap() {map.map.draw(batch);
+	}
+
+	public void drawPlayer() {player.sprite.draw(batch);
+	}
+
+	public void drawColleges() {
+		for (College college: collegeList) {
+			college.collegeSprite.setPosition(college.x, college.y);
+			college.collegeSprite.draw(batch);
+		}
+	}
+
+	public void drawCollegeStats() {
+		for (College college: collegeList) {
+			//stat for each college
+			font.draw(batch, "\nHP: " + college.HP + "\nP: " + college.POINTS+ "\nAMMO: " + college.bullets.size() + "\nC: " + college.isCaptured+ "\nA: " + college.isAttacked+ "\nAOE: " + college.isAOE, college.x+college.width, college.y+college.height);
+		}
+	}
+
+	public void drawPlayerBullets() {
+		for (int i = 0; i < player.bullets.size(); i++) {
+			player.bullets.get(i).bulletSprite.draw(batch);
+		}
+	}
+
+	public void drawCollegeBullets() {
+		for (College college: collegeList) {
+			for (int i = 0; i < college.bullets.size(); i++) {
+				college.bullets.get(i).bulletSprite.draw(batch);
+			}
+		}
+	}
+
+	//function to draw objectives:
+	//OBJECTIVES+ POINTS:
+	//Capture 2 colleges: x / 2
+	//Capture Boss college: x / 1
+	//Move with player
+
+	public void drawObjectives() {
+		font.draw(batch, "\nOBJECTIVES: \nCapture 2 colleges: " + player.captures + "/2" , player.x-500, player.y+270);
+		if (isBossUnlocked()) {
+			if (!playerWin()) {
+				font.draw(batch, "\n\n\nCapture Boss college: 0/1" , player.x-500, player.y+270);
+			} else {
+				font.draw(batch, "\n\n\nCapture Boss college: 1/1" , player.x-500, player.y+270);
+			}
+		}
+	}
+
+	public void playerInRange() {
+		for (College college: collegeList) {
+			//bullet radian was here initially:
+			if (Intersector.overlaps(college.AOE, player.rectPlayer)) {
+				college.isAOE = true;
+				System.out.println("AOE Hit");
+				if (!(college.isCaptured)) {
+					bulletDirection = new Vector2(college.x-player.x, college.y-player.y).nor();
+					bulletRadians = bulletDirection.angleRad() + 10213.2f;
+					college.shoot(bulletRadians);
+					System.out.println("AOE Hit");
+				}
+			} else {
+				college.isAOE = false;
+			}
+		}
+	}
+
+	public void bulletCollegeHit() {
+		for (College college: collegeList) {
+			if (college.isAttacked) {
+				if (!(college.isCaptured)) {
+					college.collegeHit();
+				}
+				college.isAttacked = false;
+			}
+		}
+	}
+
+	public void playerBulletHit() {
+		if (player.isAttacked) {
+			player.playerHit();
+		}
+	}
+
+	public void playerCollegeHit() {
+		for (College college: collegeList) {
+			//PLAYER-COLLEGE COLLISION
+			if (player.rectPlayer.overlaps(college.boundRect)) {
+				System.out.println("College Hit");
+				//collision:
+				player.dx = -3*player.dx/2;
+				player.dy = -3*player.dy/2;
+			}
+		}
+	}
+
+
+	//ayman modified this with player.captures in last line
+	public void collegeCaptured() {
+		for (College college: collegeList) {
+			//COLLEGE CAPTURED:
+			if ((!college.isCaptured) && college.HP == 0) {
+				System.out.println("CAPTURED");
+				college.isCaptured = true;
+				//IF STATEMENT FOR EACH COLLEGE:
+				if (college.x == 900) {
+					college.collegeSprite = college.textureAtlas.createSprite("anneLister_island_captured");
+				} else if (college.x == 1600) {
+					college.collegeSprite = college.textureAtlas.createSprite("goodricke_island_captured");
+				} else if (college.x == 2000) {
+					college.collegeSprite = college.textureAtlas.createSprite("constantine_island_captured");
+				}
+				college.HP = 5;
+				player.POINTS += college.POINTS;
+				player.captures += 1;
+			}
+		}
+	}
+	/*
+	public void collegeCaptured() {
+		for (College college: collegeList) {
+			//COLLEGE CAPTURED:
+			if ((!college.isCaptured) && college.HP == 0) {
+				System.out.println("CAPTURED");
+				college.isCaptured = true;
+				//IF STATEMENT FOR EACH COLLEGE:
+				if (college.x == 900) {
+					college.collegeSprite = college.textureAtlas.createSprite("anneLister_island_captured");
+				} else if (college.x == 1600) {
+					college.collegeSprite = college.textureAtlas.createSprite("goodricke_island_captured");
+				} else if (college.x == 2000) {
+					college.collegeSprite = college.textureAtlas.createSprite("constantine_island_captured");
+				}
+				player.POINTS += college.POINTS;
+			}
+		}
+	}
+
+	 */
+
+	//boolean checks for ending game + unlocking boss:
+	public boolean isGameEnd() {
+		return (playerWin()) || (playerLose());
+	}
+
+	public void unLockBoss() {
+		if (isBossUnlocked()) {
+			//remove barrier
+			//player.bossunlock flag = true
+			//set player boundary to unlocked boundary in update function
+			System.out.println("BOSS UNLOCKED");
+		} else {
+			//draw barrier
+		}
+	}
+
+	public boolean playerWin() {
+		return player.captures == collegeList.size();
+	}
+
+	public boolean playerLose() {
+		return player.isDead();
+	}
+
+	public boolean isBossUnlocked() {
+		return player.captures == 2;
+	}
+
+
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		//unsure what else to dispose
+	}
+}
---
+++ yorkpiratestest-master/core/src/com/ayman/screen/GameScreen.java
@@ -0,0 +1,236 @@
+package com.ayman.screen;
+
+import com.ayman.entities.AnneLister;
+import com.ayman.entities.Bullet;
+import com.ayman.game.MyGame;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen extends ScreenAdapter {
+    MyGame game;
+
+    public GameScreen(MyGame game_instance) {
+        this.game = game_instance;
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.6f, 1);
+
+        //important camera stuff (not sure what this does tho)
+        game.batch.setProjectionMatrix(game.camera.combined);
+
+        //move player sprite based on input
+        game.player.update(delta);
+        game.updatePlayerBullets(delta);
+        game.updateCollegeBullets(delta);
+
+/*
+        //ayman's code:
+        //UPDATE PLAYER BULLETS:
+        for (int i = 0; i < game.player.bullets.size(); i++) {
+
+            game.player.bullets.get(i).update(delta);
+            //COLLEGE FOR LOOP FUNCTION:
+            if (game.player.bullets.get(i).rectBullet.overlaps(game.AnneLister.boundRect)) {
+                //game.player.bullets.get(i).BulletCollide = true;
+                game.AnneLister.isAttacked = true;
+                System.out.println("BULLET COLLIDE");
+            }
+
+            if ((game.player.bullets.get(i).shouldRemove())||(game.AnneLister.isAttacked)) {
+                game.player.bullets.remove(i);
+                i--;
+                System.out.println("BULLET REMOVED");
+            }
+        }
+
+        //ayman's code:
+        //UPDATE COLLEGE BULLETS:
+        for (int i = 0; i < game.AnneLister.bullets.size(); i++) {
+
+            game.AnneLister.bullets.get(i).update(delta);
+
+            if (game.AnneLister.bullets.get(i).rectBullet.overlaps(game.player.rectPlayer)) {
+                //game.player.bullets.get(i).BulletCollide = true;
+                game.player.isAttacked = true;
+                System.out.println("PLAYER BULLET COLLIDE");
+            }
+
+            if ((game.AnneLister.bullets.get(i).shouldRemove())||(game.player.isAttacked)) {
+                game.AnneLister.bullets.remove(i);
+                i--;
+                System.out.println("PLAYER BULLET REMOVED");
+            }
+        }
+
+
+ */
+        //camera follows player after player sprite moves (player.update called)
+        game.camera.position.set(game.player.x, game.player.y, 0);
+        //ensures camera maintains aspect ratio of screen:
+        game.camera.viewportWidth = Gdx.graphics.getWidth()/1.75f;
+        game.camera.viewportHeight = Gdx.graphics.getHeight()/1.75f;
+        game.camera.update();
+
+        //begin rendering batch
+        game.batch.begin();
+
+        game.drawMap();
+        game.drawPlayer();
+        game.drawColleges();
+
+
+        //NPCs:
+        for (int i = 0; i <game.AnneLister.npcCount; i++) {
+            game.AnneLister.NPCs.get(i).npcSprite.setPosition(game.AnneLister.NPCs.get(i).x, game.AnneLister.NPCs.get(i).y);
+            game.AnneLister.NPCs.get(i).npcSprite.draw(game.batch);
+            game.Constantine.NPCs.get(i).npcSprite.setPosition(game.Constantine.NPCs.get(i).x, game.Constantine.NPCs.get(i).y);
+            game.Constantine.NPCs.get(i).npcSprite.draw(game.batch);
+            game.Goodricke.NPCs.get(i).npcSprite.setPosition(game.Goodricke.NPCs.get(i).x, game.Goodricke.NPCs.get(i).y);
+            game.Goodricke.NPCs.get(i).npcSprite.draw(game.batch);
+        }
+
+        //GAME STATS:
+        game.font.draw(game.batch, "\nx: "+ game.player.x+"    y: "+game.player.y+" \n   shipAngle: "+game.player.angle+" \n shipR: "+game.player.radians +" \n DX: "+game.player.dx + " DY: "+game.player.dy , 700, 700);
+        game.font.draw(game.batch, "\nHP: " + game.player.HP + "\nP: " + game.player.POINTS+ "\nAMMO: " + game.player.bullets.size() + "\nSCREENX: " + Gdx.graphics.getWidth(), game.player.x+game.player.width, game.player.y+game.player.height);
+
+        //stat for each college
+        game.drawCollegeStats();
+
+        //draw Objectives:
+        game.drawObjectives();
+
+        /*
+        //draw bullets
+        for (int i = 0; i < game.player.bullets.size(); i++) {
+            game.player.bullets.get(i).bulletSprite.draw(game.batch);
+            System.out.println("BULLET DRAWN");
+        }
+
+         //DRAW COLLEGE BULLETS:
+        for (int i = 0; i < game.AnneLister.bullets.size(); i++) {
+            game.AnneLister.bullets.get(i).bulletSprite.draw(game.batch);
+            System.out.println("COLLEGE BULLET DRAWN");
+        }
+        */
+
+        game.drawPlayerBullets();
+        game.drawCollegeBullets();
+
+        game.batch.end();
+
+        //ship controls:
+
+        /*
+        //PLAYER-PLAYER COLLISION (player ship class method):
+        if (game.player.rectPlayer.overlaps(game.player2.rectPlayer)) {
+            System.out.println("Player Hit");
+            game.player.dx = -3*game.player.dx/2;
+            game.player.dy = -3*game.player.dx/2;
+
+            game.player2.dx = -3*game.player2.dx/2;
+            game.player2.dy = -3*game.player2.dy/2;
+        }
+        */
+
+        ///////////////////GAME LOGIC//////////////////////////////////////////////////////////////////////
+
+
+        /*
+        //update college bullet angle before PLAYER IN RANGE below:
+        //bulletDirection = new Vector2(game.AnneLister.x-game.player.x, game.AnneLister.y-game.player.y).nor();
+        //bulletRadians = bulletDirection.angleRad() + 10213.2f;
+
+        //PLAYER IN RANGE (IMPLEMENT FOR LOOP ON COLLEGE LIST FOR ALL BELOW):
+
+        if (Intersector.overlaps(game.AnneLister.AOE, game.player.rectPlayer)) {
+            game.AnneLister.isAOE = true;
+            System.out.println("AOE Hit");
+            if (!(game.AnneLister.isCaptured)) {
+                game.AnneLister.shoot(bulletRadians);
+            }
+        } else {
+            game.AnneLister.isAOE = false;
+        }
+
+         */
+        game.playerInRange();
+
+/*
+        //BULLET COLLEGE COLLISION:
+        if (game.AnneLister.isAttacked) {
+            if (!(game.AnneLister.isCaptured)) {
+                game.AnneLister.collegeHit();
+            }
+            game.AnneLister.isAttacked = false;
+        }
+
+ */
+        game.bulletCollegeHit();
+/*
+        //PLAYER BULLET COLLISION:
+        if (game.player.isAttacked) {
+            game.player.playerHit();
+        }
+
+        //PLAYER-COLLEGE COLLISION
+        if (game.player.rectPlayer.overlaps(game.AnneLister.boundRect)) {
+            System.out.println("College Hit");
+            //collision:
+            game.player.dx = -3*game.player.dx/2;
+            game.player.dy = -3*game.player.dy/2;
+        }
+
+ */
+        game.playerBulletHit();
+        game.playerCollegeHit();
+
+        /*
+        //PLAYER DEAD:
+        if (game.player.isDead()) {
+            System.out.println("DEAD");
+            //called game function to restart game at end screen)
+            //game.player.x= 600;
+            //game.player.y = 600;
+            //game.player.HP = 10;
+            //game.player.dx = 0;
+            //game.player.dy = 0;
+            game.setScreen(new EndScreen(game));
+        }
+/*
+        //COLLEGE CAPTURED:
+        //AnneLister captured:
+        if ((!game.AnneLister.isCaptured) && game.AnneLister.HP == 0) {
+            System.out.println("CAPTURED");
+            game.AnneLister.isCaptured = true;
+            //this doesnt render in game screen for some reason???
+            game.AnneLister.collegeSprite = game.AnneLister.textureAtlas.createSprite("anneLister_island_captured");
+            game.AnneLister.HP = 10;
+            game.player.POINTS += game.AnneLister.POINTS;
+        }
+
+ */
+        game.collegeCaptured();
+
+        //TASK CHECKLIST:
+        if(game.isGameEnd()) {
+            game.setScreen(new EndScreen(game));
+        }
+
+        game.unLockBoss();
+
+
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+}
